11ab9a5f6a393785a936db63b89508a55e56672e
Guilherme Guimaraes <technestowl@gmail.com>
2024-10-17 15:49:22 -0400

    added zeroz array to problem #1

--- Form1.cs
+++ Form1.cs
@@ -14,7 +14,17 @@ namespace Arrays
             //1.) Create an Empty Array of 100 integers.
             //2.) Fill this Array with all 0’s(zeros).Use a Loop. 
             //3.) Print out the 100 values stored in the Array to the Console(Output) Window.Use a Loop.
+            
+            //initializng my empty array of 100 integers
+            int[] zerosArray = new int[100];
 
+            //looping through and setting each i to 0
+            for (int i = 0;i < zerosArray.Length; i++)
+            {
+                zerosArray[i] = 0;
+            }
+            //outputing to debubbing console
+            System.Diagnostics.Debug.WriteLine(zerosArray);
 
 
         }
